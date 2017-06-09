bcc7c65142b86528df730bfe9f54b026fd0d44e9
Kaitlynn Hong <kaitlynnhong@Kaitlynns-MacBook-Pro.local>
2017-06-09 15:22:50 -0700

    wrote js file to display map

--- mbta/mbta_script.js
+++ mbta/mbta_script.js
@@ -1,6 +1,6 @@
-function display_map()
+function render_map()
 {
-    var south_station = new google.maps.LatLng({lat:42.351877600, lng:-71.055104200});
+    var south_station = new google.maps.LatLng({lat: 42.351877600, lng: -71.055104200});
 
     var options = {
 					zoom: 15,
@@ -8,8 +8,8 @@ function display_map()
 					mapTypeId: google.maps.MapTypeId.ROADMAP
 				};
     
-    var map = new.google.Map(document.getElementById=("map_canvas"), options);
-
+    var map = new google.maps.Map(document.getElementById=("map_canvas"), options);
+    
     var south_marker = new google.maps.Marker({
         position: south_station,
         title: "South Station, Boston, MA"
@@ -17,7 +17,7 @@ function display_map()
     south_marker.setMap(map);
 
     var infowindow = new google.maps.InfoWindow();
-    google.maps.event.addListener(south_marker, 'click', function(){
+    google.maps.event.addListener(south_marker, 'click', function() {
         infowindow.setContent(south_marker.title);
         infowindow.open(map, south_marker);
     });
