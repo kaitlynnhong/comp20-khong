28804dfcb31fea2ccab7ec759eeb864ad0534798
Kaitlynn Hong <kaitlynnhong@Kaitlynns-MacBook-Pro.local>
2017-06-16 00:43:41 -0400

    Starting messages part 2

--- messages/lab.js
+++ messages/lab.js
@@ -1,23 +1,3 @@
-function request_messages()
-{
-    //new XMLHttpRequest object 
-    var request = new XMLHttpRequest();
-    request.open("GET", "data.json", true);
-
-    request.onreadystatechange = function() {
-        if (request.readyState == 4 && request.status == 200) 
-        {
-            messages = JSON.parse(request.responseText);
-            parsed = "";
-            for (i = 0; i < messages.length; i++)
-            {
-                parsed += "<p>" + messages[i].content + " " + messages[i].username + "</p>";
-            }
-            document.getElementById("messages").innerHTML = parsed;
-        }
-    };
-    
-    request.send();
-}
+//your js here 
 
 
