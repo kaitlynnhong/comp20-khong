48ab2508fbf5bed6e252c9e76f176eb10ff4b6df
Kaitlynn Hong <kaitlynnhong@Kaitlynns-MacBook-Pro.local>
2017-06-16 01:22:23 -0400

    added span tag for style

--- messages/lab.js
+++ messages/lab.js
@@ -1,17 +1,19 @@
-function request_messages()
+function parse()
 {
     //new XMLHttpRequest object 
     var request = new XMLHttpRequest();
     request.open("GET", "data.json", true);
 
+    //onreadystatechange function parses data.json
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) 
         {
             messages = JSON.parse(request.responseText);
             parsed = "";
+            
             for (i = 0; i < messages.length; i++)
             {
-                parsed += "<p>" + messages[i].content + " " + messages[i].username + "</p>";
+                parsed += "<p><span class='question'>" + messages[i].content + "</span>" + " " + messages[i].username + "</p>";
             }
             document.getElementById("messages").innerHTML = parsed;
         }
